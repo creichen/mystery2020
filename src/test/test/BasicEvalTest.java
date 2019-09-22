6fb156d60e06edf0967b311f8ed013dc350fe34b
Christoph Reichenbach <creichen@gmail.com>
2019-09-22 09:01:53 +0000

    Added support for conditionals

--- src/test/test/BasicEvalTest.java
+++ src/test/test/BasicEvalTest.java
@@ -14,4 +14,32 @@ public class BasicEvalTest extends EvaluationTest {
 				"PRINT 9; PRINT 1",
 				"9", "1");
 	}
+	
+	public void
+	testCondTrue0() {
+		forallConfigurationsTestOutput(
+				"IF 1 THEN\n PRINT 1\n ELSE\n PRINT 0 END\n",
+				"1");
+	}
+	
+	public void
+	testCondTrue1() {
+		forallConfigurationsTestOutput(
+				"IF -1 THEN PRINT 1 ELSE PRINT 0 END",
+				"1");
+	}
+	
+	public void
+	testCondTrue2() {
+		forallConfigurationsTestOutput(
+				"IF 2 THEN PRINT 1 ELSE PRINT 0 END",
+				"1");
+	}
+	
+	public void
+	testCondFalse() {
+		forallConfigurationsTestOutput(
+				"IF 0 THEN PRINT 1 ELSE PRINT 0 END",
+				"0");
+	}
 }
