a11ba4150508e5dfc830fce955ac885e66d8c4dd
Christoph Reichenbach <creichen@gmail.com>
2019-10-09 07:06:46 +0000

    Added new pretty-printing feature to exceptions

--- src/main/mystery2020/MysteryException.java
+++ src/main/mystery2020/MysteryException.java
@@ -26,4 +26,24 @@ public abstract class MysteryException extends RuntimeException {
 	public String toString() {
 		return this.key + " " + this.line_nr + (this.message.length() == 0 ? "" : (" " + this.message));
 	}
+
+	public void printProgram(String body) {
+		final String[] lines = body.split("\n");
+		String pad = "";
+		while (pad.length() < this.key.length()) {
+			pad += " ";
+		}
+		final String marker = " > ";
+		pad +=                "   ";
+		for (int i = 0; i < lines.length; i++) {
+			int current_line_nr = i + 1;
+			if (current_line_nr == this.line_nr) {
+				System.err.print(this.key + marker);
+			} else {
+				System.err.print(pad);
+			}
+			System.err.print(String.format("%-4d", current_line_nr));
+			System.err.println(lines[i]);
+		}
+	}
 }
